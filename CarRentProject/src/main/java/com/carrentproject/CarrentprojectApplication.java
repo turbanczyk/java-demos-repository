bd69d7bde7858f76b3f6510a714125fe374f867a
tomeku <tomeku@air-tomek>
2022-02-10 12:23:51 +0100

    Project initialization

---
+++ CarRentProject/src/main/java/com/carrentproject/CarrentprojectApplication.java
@@ -0,0 +1,13 @@
+package com.carrentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentprojectApplication.class, args);
+	}
+
+}
